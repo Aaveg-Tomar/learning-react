8ffc6008ba3e3db74540f7244c7096e8a8e78e04
Aaveg-Tomar <tomaraaveg@gmail.com>
2022-07-24 11:51:21 +0530

    function base components

--- my-app/src/Components/User/user.js
+++ my-app/src/Components/User/user.js
@@ -1,12 +1,27 @@
-function User(props){
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './user.css';
 
+
+
+
+function User(props){
+    const {data}=props;
+        console.log(props.data)
     return(
-        <div>
-           {/*  <h4>My name is Rohan</h4>  */}
-           <h4>My name is {props.name} and my age is {props.age}  </h4>
-        </div>
-           
-           
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={data.picture} />
+        <Card.Body>
+          <Card.Title>Card Title { ` ${data.title} ${data.firstName} ${data.lastName} ` }</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+    
     );
 }
 
--- my-app/src/Components/Users/users.js
+++ my-app/src/Components/Users/users.js
@@ -1,33 +1,110 @@
+import React from "react";
 import User from "../User/user"
+import './users.css'
 
-const users = [{name:"Aman", age:"19" },{name:"Abhishek", age:"18"}]
-function Users()
-{
-    return (
-        <div>
-            <h2>This is user components</h2>
-            <User/>
-            <User name = "Utkarsh" age = "27"/>
-            <User name = "Rohan" age = "25"/>
+const users = [
+    {
+        "id":"60d0fe4f5311236168a109ca",
+        "title":"ms",
+        "firstNAme":"Sara",
+        "lastName":"Adersen",
+        "picture":"https://randomuser.me/api/portraits/women/58.jpg"
 
-            {/* This is JavaScript  */}
+    },
+    {
+        "id":"60d0fe4f5311236168a109cb",
+        "title":"miss",
+        "firstNAme":"Edita",
+        "lastName":"Vestering",
+        "picture":"https://randomuser.me/api/portraits/women/89.jpg"
+    },
+    {
+        "id":"60d0fe4f5311236168a109cc",
+        "title":"ms",
+        "firstNAme":"Adina",
+        "lastName":"Barbosa",
+        "picture":"https://randomuser.me/api/portraits/women/28.jpg"
+    },
+    {
+        "id":"60d0fe4f5311236168a109ca",
+        "title":"ms",
+        "firstNAme":"Sara",
+        "lastName":"Adersen",
+        "picture":"https://randomuser.me/api/portraits/women/58.jpg"
 
-            {
-                 users.map((user)=>{
+    },
+    {
+        "id":"60d0fe4f5311236168a109cb",
+        "title":"miss",
+        "firstNAme":"Edita",
+        "lastName":"Vestering",
+        "picture":"https://randomuser.me/api/portraits/women/89.jpg"
+    },
+    {
+        "id":"60d0fe4f5311236168a109cc",
+        "title":"ms",
+        "firstNAme":"Adina",
+        "lastName":"Barbosa",
+        "picture":"https://randomuser.me/api/portraits/women/28.jpg"
+    },
 
-                     return <User name={user.name} age={user.age} />
-                })
-
-            }
-                
-                    
+]
 
 
-                
-            
-        </div>
+// class Users extends React.Component {
+
+//     constructor() {
+//         super();
+//     }
+
+
+//     render()
+//     {
+//         return(
+
+//             <div>
+//                 <h>Empolyee List</h>
+//                 <div className="usersDiv">
+
+//                     {/* javaScript */}
+
+//                     {
+//                        users.map((user)=>{
+//                          return <User data = {user}/>
+//                        })
+//                     }
+
+//                 </div>
+//             </div>
+//         )
+//     }
+
+// }
+
+
+function Users(){
+    return(
+                     <div>
+                        <h>Empolyee List</h>
+                        <div className="usersDiv">
         
-    );
-}   
+                            {/* javaScript */}
+        
+                            {
+                               users.map((user)=>{
+                                 return <User data = {user}/>
+                               })
+                            }
+        
+                        </div>
+                    </div>
+                )
+}
+
+
+
+
+
+
 
 export default Users;
